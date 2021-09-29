defc84cd15e29ea24c51806dd76d8021accd3176
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-29 22:03:05 +0530

    API documentation created with swagger-ui-express

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
 const cors = require("cors");
 const { mail } = require("./server/routers");
+
+// API Doc imported
+const swaggerDocument = require("./documentation/index.json");
 
 // Initialize the express application
 const app = express();
@@ -16,6 +20,9 @@ app.get("/", (req, res) => res.send({ data: "Node Mailer Express API" }));
 
 // Route for handing email work
 app.use("/mail", mail);
+
+// Create api documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
