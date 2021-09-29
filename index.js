48bbd79714b59cd7db2c800d8946601b7dde5423
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-29 21:00:49 +0530

    Route for handing email work created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { mail } = require("./server/routers");
+
+// Initialize the express application
 const app = express();
 
 // Define Our middleware
@@ -10,6 +13,9 @@ app.use(express.json());
 
 // Basic Get Route Created
 app.get("/", (req, res) => res.send({ data: "Node Mailer Express API" }));
+
+// Route for handing email work
+app.use("/mail", mail);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
---
+++ server/routers/mail.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { transporter } = require("../config");
+const router = express.Router();
+
+router
+  .route("/send")
+  .get((req, res) => {
+    res.send({
+      data: `${req.baseUrl}${req.url} is a post route handling email related stuff`,
+    });
+  })
+  .post(async (req, res) => {
+    const { email, subject, content } = req.body;
+    try {
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject,
+        html: content,
+      };
+      await transporter.sendMail(mailOptions);
+      res.send({
+        data: `Mail sent successfully to ${email}`,
+        status: 200,
+      });
+    } catch (error) {
+      res.send({
+        error,
+        status: 404,
+      });
+    }
+  });
+
+module.exports = router;
